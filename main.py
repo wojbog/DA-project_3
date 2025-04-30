add343914da0d0c1ff230fe4052df5c46842a454
shhhQuiettt <krzysztof.skro@gmail.com>
2025-04-30 14:23:36 +0200

    Add shap

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sklearn.linear_model import LogisticRegression
+import shap
+# xkboost
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from yellowbrick.classifier import ClassificationReport, ROCAUC
 import numpy as np
 import pandas as pd
-from explainerdashboard import ClassifierExplainer, ExplainerDashboard
 
 # train split
 from sklearn.model_selection import train_test_split
@@ -24,35 +26,32 @@ X = df.drop(columns=["category"])
 y = (df["category"] - 1) // 2
 
 
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=0xC0FFEE)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0xC0FFEE)
 
 model = LogisticRegression()
-model.fit(train_X, train_y)
+model.fit(X_train, y_train)
+
+classes = ["Category 1", "Category 2"]
 
 visualizers = [
-    ClassificationReport(model, classes=[0, 1]),
-    ROCAUC(model, classes=[0, 1], size=(1080, 720)),
+    ClassificationReport(model, classes=classes, recall=False),
+    ROCAUC(model, classes=classes, size=(1080, 720)),
 ]
 
-explainer = ClassifierExplainer(model, test_X, test_y)
+# explainer = ClassifierExplainer(model, test_X, test_y)
 # ExplainerDashboard(explainer).run()
-# for v in visualizers:
-#     v.fit(train_X,train_y)
-#     v.score(test_X, test_y)
-#     v.show()
+for v in visualizers:
+    v.fit(X_train, y_train)
+    v.score(X_test, y_test)
+    v.show()
 
-# # # y_hat = model.predict(data)
-# # # y_hat_proba = model.predict_proba(data)
-# # # print(y_hat)
-# # # accuracy = accuracy_score(labels, y_hat)
-
-# # # f1 = f1_score(labels, y_hat)
-# # # print((labels == y_hat).sum() / len(labels))
-# # # print(f"Accuracy: {accuracy}")
-# # # print(f"F1 Score: {f1}")
-# # # print(f"ROC AUC Score: {roc_auc_score(labels, y_hat_proba[:, 1])}")
+# print coefficients
+coefs = model.coef_[0]
 
 
+explainer = shap.Explainer(model, X_train)
+shap_values = explainer(X_test[:3])
+shap.plots.waterfall(shap_values[0])
 
 
 
